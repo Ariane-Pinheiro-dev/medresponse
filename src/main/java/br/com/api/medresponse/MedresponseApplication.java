e7c2af38b38bde993d2b06a09c3046fe23b0fa4c
Ariane Pinheiro <arianepinheirodev@gmail.com>
2026-09-20 17:22:14 -0300

    inicia projeto

---
+++ src/main/java/br/com/api/medresponse/MedresponseApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.medresponse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedresponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedresponseApplication.class, args);
+	}
+
+}
