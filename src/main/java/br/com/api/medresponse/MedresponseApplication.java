71bab0a188e59ea0b21714413460dc82333703ad
Ariane Pinheiro <arianepinheirodev@gmail.com>
2026-09-22 15:22:43 -0300

    configura projeto

--- src/main/java/br/com/api/medresponse/MedresponseApplication.java
+++ src/main/java/br/com/api/medresponse/MedresponseApplication.java
@@ -2,9 +2,17 @@ package br.com.api.medresponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class MedresponseApplication {
+
+	public String getHomeTest(){
+		return "MedResponse - API HOME";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MedresponseApplication.class, args);
